c2042f1b2a78bffb9aa3c1d2a8f699a302238a14
graef <simonjn45@gmail.com>
2025-05-27 21:25:46 +0200

    feat: made 2015/4 clearer

--- 2015/4/4.js
+++ 2015/4/4.js
@@ -1,21 +1,22 @@
-import { MD5 } from "./md5.js"
+import { md5 } from "jsr:@takker/md5";
+import { encodeHex } from "jsr:@std/encoding@1/hex";
 
 const input = (await Deno.readTextFile("./4.txt"))
 
 let i = 0
 while (true) {
-  const hash = MD5(input + i);
+  const hash = encodeHex(md5(input + i));
   if (hash.substring(0, 5) == "00000") {
     break;
   }
   i++;
 }
-cd
+
 console.log(i)
 
 i = 0
 while (true) {
-  const hash = MD5(input + i);
+  const hash = encodeHex(md5(input + i));
   if (hash.substring(0, 6) == "000000") {
     break;
   }
